106a4084b3879e7e4ab63b42e47e4e6fe1b556ab
Carlos Ramírez <caramirezal@users.noreply.github.com>
2017-10-03 19:59:09 -0500

    no terminado

--- scripts/pueblaIDs.R
+++ scripts/pueblaIDs.R
@@ -1,39 +1,70 @@
 
 puebla <- read.csv("DataGeograficaPuebla.csv",stringsAsFactors = FALSE)
+puebla <- puebla[!duplicated(puebla),]
+
+puebla$index <- 1:length(puebla$Estado)
+
+puebla$filteredColonia <- gsub("[^[:alpha:]]","",puebla$Colonia)
+puebla$filteredColonia <- gsub("_","",puebla$filteredColonia)
+puebla$filteredColonia <- iconv(puebla$filteredColonia, to='ASCII//TRANSLIT')
+letters <- with(puebla, lapply(1:length(filteredColonia), 
+                               function(x) strsplit(filteredColonia[x],"") ) )
+sortedLetters <- lapply(1:length(letters), 
+                            function(x) toupper(letters[[x]][[1]]) )
+#sortedLetters <- lapply(1:length(letters), 
+#                            function(x) sort(unique(sortedLetters[[x]])) )
+sortedChars <- sapply(1:length(sortedLetters), 
+                        function(x) paste(sortedLetters[[x]],collapse = ""))
+puebla$sortedLetters <- sortedChars
+
+puebla$ids <- with(puebla, paste(substr(Estado,1,2),substr(Municipio,1,2),sep="."))
+puebla$ids <- toupper(puebla$ids)
+#puebla$ids <- sapply(1:length(puebla[,1]), 
+#                     function(x) paste(puebla$ids[x],
+#                                       puebla$sortedLetters[x],
+#                                       #toupper(substr(sortedLetters[x],1,1)),
+#                                       sep="."))
+#
+firstLetter <- iconv(substring(puebla$Colonia,1,1),to="ASCII//TRANSLIT")
+secLetter <- sub(".* ","",puebla$Colonia)
+secLetter <- iconv(substr(secLetter,1,1),to="ASCII//TRANSLIT")
+idCol <- sapply(1:length(secLetter),function(x) paste(firstLetter[x],secLetter[x],sep=""))
+puebla$idCol <- idCol
+puebla$ID <- sapply(1:length(secLetter),function(x) paste(puebla$ids[x],puebla$idCol[x],sep="."))
 
 
-estado <- substr(puebla$Estado,1,2)
-municipio <- substr(puebla$Municipio,1,2)
-puebla$Colonia <- gsub("\\(","",puebla$Colonia)
-puebla$Colonia <- gsub("\\)","",puebla$Colonia)
-puebla$Colonia <- gsub("\\.","",puebla$Colonia)
-first <- substr(puebla$Colonia,1,2)
-second <- substr(gsub(".* ","",puebla$Colonia),1,1)
-
-paste(estado[1],municipio[1],paste(first[1],second[1],sep=""),sep=".")
-
-ids <- sapply(1:length(puebla[,1]), function(x) paste(estado[x],
-                                                      municipio[x],
-                                                      first[x],
-                                                      sep="."))
-ids <- toupper(ids)
-puebla$ids <- ids
-
-##puebla <- puebla[order(puebla$ids),]
-
-usedIDs <- puebla$ids[1]
-for (i in 1:length(puebla[,1])) {
-        newID <- puebla$ids[i]
-     while ( ( newID %in% usedIDs  ) || 
-             substr(newID,8,8) == " "
-             || grepl(substr(newID,8,8),"0123456789" ) )  {
-             randomNum <- sample(1:nchar(puebla$Colonia[i]),1)
-             randomchr <- substr(puebla$Colonia[i],randomNum,randomNum)
-             newID <- paste(substr(newID,1,7),randomchr,sep="")
-             newID <- toupper(newID)
-     }
-        usedIDs <- c(usedIDs,newID)
-        cat(i,": ",usedIDs[i],"\n")
+puebla$finalID <- rep("--.--.--",length(puebla$Estado))
+bag <- puebla$finalID[1]
+for (i in 1:length(puebla$ids)) {
+      if ( ! ( puebla$ID[i] %in% bag ) ){
+           puebla$finalID[i] <- puebla$ID[i]
+           bag <- c(bag,puebla$ID[i])
+      }
 }
+
+for (i in 1:length(puebla$ids)) {
+        if (puebla$finalID[i] == "--.--.--") {
+              colonia1 <- strsplit(puebla$sortedLetters[i-1],"")
+              colonia2 <- strsplit(puebla$sortedLetters[i],"")
+              difLetters <- intersect(colonia1,colonia2)
+              if ( length(difLetters) > 0 ) {
+                    counter <- 1
+                    newID <- paste(substr(puebla$ID[i],1,7),difLetters[counter],sep="")
+                    while ( counter < length(difLetters) && newID %in% bag ) {
+                          newID <- paste(substr(puebla$ID[i],1,7),difLetters[counter],sep="")
+                          counter <- counter + 1
+                    }
+                    if ( ! ( newID %in% bag ) ) {
+                          puebla$finalID[i] <- newID
+                    }
+              }
+
+
+        } 
+}
+
+#puebla$ids <- sapply(1:length(puebla[,1]), 
+#                     function(x) paste(puebla$ids[x],puebla$sortedLetters[x],sep="."))
+
 
 
