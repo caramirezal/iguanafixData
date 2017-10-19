e7811c68f56a4dbf6b65ac1ad05ef334e76b3866
caramirezal <cramireza@ciencias.unam.mx>
2017-10-19 15:02:51 -0500

    Estado y municipio ID creator and automating anticipos backup

--- scripts/IDCreator.R
+++ scripts/IDCreator.R
@@ -5,6 +5,7 @@
 ## MU - Municipio
 ## CO - Colonia
 
+library(dplyr)
 
 puebla <- read.csv("DataGeograficaPuebla.csv",stringsAsFactors = FALSE)
 
@@ -246,3 +247,78 @@ for (i in 1:length(puebla$Estado)) {
 
 pueblaIds <- puebla[,c("Estado","Municipio","Colonia","id")]
 write.csv(pueblaIds,file = "PUEBLAIDS.csv")
+
+##################################################################################################################################
+
+## definition of the ids para estados y municipios
+##
+
+
+## reading data
+ids <- read.csv("pueblaIDs.csv")
+ids <- select(ids, Estado:Municipio)
+idsRegion <- unique(ids)
+
+
+
+## getting Estado part of the name
+idsRegion <- mutate(idsRegion,EstadoFormatted=filterStrings(Estado) )
+
+## getting municipio
+idsRegion <- mutate(idsRegion,MunicipioFormatted=filterStrings(Municipio) )
+
+
+
+## id assignment
+bag <- NULL
+idsRegion$"id" <- rep("--.--",length(idsRegion$Estado))
+for (i in 1:length(idsRegion$Estado)) {
+        ## try to assign first and second initial
+        ## to .CO part of the ID
+        estadoID <- substr(idsRegion$EstadoFormatted[i],1,2)
+        municipioID <- substr(idsRegion$MunicipioFormatted[i],1,2)
+        newId <- paste(estadoID,municipioID,sep=".")
+        allreadyInBag <- newId %in% bag
+        if ( ! allreadyInBag ) {
+                bag <- c(bag,newId)
+                idsRegion$id[i] <- newId 
+                cat(i, newId,"\n")
+        }
+        if ( allreadyInBag ) {
+                for (j in 1:nchar(idsRegion$MunicipioFormatted[i])) {
+                        newId <- paste(substr(idsRegion$MunicipioFormatted[i],1,1),
+                                       substr(idsRegion$MunicipioFormatted[i],j,j),sep="")
+                        newId <- paste("PU",newId,sep=".")
+                        allreadyInBag <- newId %in% bag
+                        if ( ! allreadyInBag ) {
+                                bag <- c(bag,newId)
+                                idsRegion$id[i] <- newId 
+                                cat(i, newId,"\n")
+                                break 
+                        }
+
+                }
+        }
+        if ( allreadyInBag ) {
+                for (j in 1:nchar(idsRegion$MunicipioFormatted[i])) {
+                        for (k in 1:nchar(idsRegion$MunicipioFormatted[i])) {
+                                newId <- paste(substr(idsRegion$MunicipioFormatted[i],j,j),
+                                               substr(idsRegion$MunicipioFormatted[i],k,k),sep="")
+                                newId <- paste("PU",newId,sep=".")
+                                allreadyInBag <- newId %in% bag
+                                if ( ! allreadyInBag ) {
+                                        bag <- c(bag,newId)
+                                        idsRegion$id[i] <- newId 
+                                        cat(i, newId,"\n")
+                                        break 
+                                }
+                        }
+                        if (  ! allreadyInBag ) break
+                }
+        }
+}
+
+
+## writting results
+ids <- select(idsRegion, -(EstadoFormatted:MunicipioFormatted))
+write.csv(ids,file = "idsRegion.csv",row.names = FALSE)
