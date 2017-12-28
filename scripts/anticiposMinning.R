276c1ac884213116ebb993ab2520d04b0d88ae60
caramirezal <cramireza@ciencias.unam.mx>
2017-12-28 16:05:12 -0600

    extract all quotes

--- scripts/anticiposMinning.R
+++ scripts/anticiposMinning.R
@@ -1,4 +1,4 @@
- 
+library(dplyr) 
 setwd("~/scripts/iguanafixData/data/Sent/")
 fileNames <- list.files()
 csvs <- gsub(".xlsx",".csv",fileNames)
@@ -12,5 +12,57 @@ for (i in 1:length(fileNames)) {
 command <- "mv *.csv ~/scripts/iguanafixData/data/anticipos_txt"
 system(command)
 
+
+setwd("~/scripts/iguanafixData/data/anticipos_txt/")
 fileNames <- list.files("~/scripts/iguanafixData/data/anticipos_txt/")
-head(fileNames)
+
+lines <- readLines(fileNames[1])
+
+extractJobId <- function(csv) {
+        lines <- readLines(csv)
+        jobId <- lines[grepl("de Trabajo",lines)][1]
+        jobId <- gsub(".* de Trabajo","",jobId)
+        jobId <- gsub("*.,","",jobId)
+        jobId <- gsub(" ","",jobId)
+        jobId        
+}
+
+extractClientId <- function(csv){
+        csv <- readLines(csv)
+        clienteId <- csv[grepl("Cliente",csv)][1]
+        clienteId <- gsub('.*Cliente',"",clienteId)
+        clienteId <- gsub('N°.*',"",clienteId)
+        clienteId <- gsub(",","",clienteId)
+        clienteId <- gsub('"','',clienteId)
+        clienteId        
+}
+
+extractTotal <- function(csv){
+        lines <- readLines(csv)
+        total <- lines[grepl("IMPORTE TOTAL",lines)][1]
+        total <- gsub("IMPORTE TOTAL","",total)
+        total <- gsub(",","",total)
+        total <- gsub('\"','',total)
+        total
+}
+
+
+jobIds <- sapply(fileNames,function(i) extractJobId(i))
+jobIds <- as.numeric(as.character(jobIds))
+jobIds[is.na(jobIds)] <- 0 
+clientIds <- sapply(fileNames,function(i) extractClientId(i))
+totals <- sapply(fileNames, function(i) extractTotal(i))
+totals <- as.numeric(as.character(totals))
+totals[is.na(totals)] <- 0
+res <- data.frame("job_id"=jobIds,
+                  "client"=clientIds,
+                  "total"=totals,
+                  "file"=fileNames)
+
+write.csv(res,
+          file="~/scripts/iguanafixData/data/anticipos.csv",
+          row.names = FALSE)
+
+
+length(fileNames)
+nrow(res)
