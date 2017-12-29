f40cf8cf3c01648400d3885dae6af692744e2dce
caramirezal <cramireza@ciencias.unam.mx>
2017-12-28 22:14:54 -0600

    Add date

--- scripts/anticiposMinning.R
+++ scripts/anticiposMinning.R
@@ -46,7 +46,31 @@ extractTotal <- function(csv){
         total
 }
 
+extractMontoPro <- function(csv){
+        lines <- readLines(csv)
+        montoPro <- lines[grepl("Mano de obra",lines)][1]
+        montoPro <- gsub("Mano de obra","",montoPro)
+        montoPro <- gsub(",","",montoPro)
+        montoPro <- gsub('\"',"",montoPro)
+        montoPro <- as.character(montoPro)
+        montoPro <- as.numeric(montoPro)
+        montoPro[is.na(montoPro)] <- 0
+        montoPro
+}
 
+extractDate <- function(csv){
+        lines <- readLines(csv)
+        date <- lines[grepl("Domicilio",lines)][1]
+        date <- gsub(".*Fecha","",date)
+        date <- gsub('\"','',date)
+        date <- gsub(',','',date)
+        date <- gsub(' ','',date)
+        date <- strptime(date,format="%Y/%m/%d")
+        date <- as.character(date)
+        date
+}
+
+date <- sapply(fileNames,function(i) extractDate(i))
 jobIds <- sapply(fileNames,function(i) extractJobId(i))
 jobIds <- as.numeric(as.character(jobIds))
 jobIds[is.na(jobIds)] <- 0 
@@ -54,9 +78,13 @@ clientIds <- sapply(fileNames,function(i) extractClientId(i))
 totals <- sapply(fileNames, function(i) extractTotal(i))
 totals <- as.numeric(as.character(totals))
 totals[is.na(totals)] <- 0
-res <- data.frame("job_id"=jobIds,
+montoPro <- sapply(fileNames, function(i) extractMontoPro(i))
+        
+res <- data.frame("date"=date,
+                  "job_id"=jobIds,
                   "client"=clientIds,
                   "total"=totals,
+                  "montoPro"=montoPro,
                   "file"=fileNames)
 
 write.csv(res,
@@ -64,5 +92,3 @@ write.csv(res,
           row.names = FALSE)
 
 
-length(fileNames)
-nrow(res)
